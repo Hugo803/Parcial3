c2a3e22a31ae299e1524aa36f90bc48ba08afe83
Hugo803 <hugocuellar76@gmail.com>
2022-04-24 22:49:34 -0600

    Parcial3
    Flutter

---
+++ parcial3/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:pacial03/api/parcial03service.dart';
+import 'package:pacial03/ui/cajaPersonaje.dart';
+import 'package:pacial03/ui/spinnerwidget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Parcial 3',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: apipage(),
+    );
+  }
+}
+
+class apipage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 238, 255, 0),
+          elevation: 0,
+          title: Padding(
+            padding:
+                const EdgeInsets.only(left: 10, bottom: 20, top: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Parcial 3",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 0, 255, 0),
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/image/st3.jpeg'),
+                      fit: BoxFit.fill,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: FutureBuilder(
+            future: Parcial03service.getPeople(),
+            builder: (BuildContext contex, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (contex, index) {
+                    var personaje = snapshot.data![index];
+                    return CajaPersonaje(personajes: personaje);
+                  },
+                );
+              } else {
+                return SpinnerWidget();
+              }
+            }));
+  }
+}
